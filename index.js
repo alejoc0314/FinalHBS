44e76f784269bd5dfe1915c9c36fcbdf65e4f0bd
alejoc0314 <alejoc0314@gmail.com>
2023-06-19 21:13:06 -0500

    fix2.0 prod repetido

--- index.js
+++ index.js
@@ -354,15 +354,16 @@ app.post('/registroPedidos', (req, res) => {
         const productoExistente = listaDetalle.find(detalle => detalle._id === _id);
         if (productoExistente) {
             const mensajeError = 'El producto ya ha sido agregado.';
-            res.status(400).json({ error: mensajeError }); 
+            res.status(400).json({ error: mensajeError });
 
-        const precioTotal = productoEncontrado.precioVenta * cantidad;
-        const productoFinal = {
-            ...productoEncontrado,
-            cantidad: cantidad,
-            precioTotal: precioTotal
-        };
-        listaDetalle.push(productoFinal);
+            const precioTotal = productoEncontrado.precioVenta * cantidad;
+            const productoFinal = {
+                ...productoEncontrado,
+                cantidad: cantidad,
+                precioTotal: precioTotal
+            };
+            listaDetalle.push(productoFinal);
+        }
     }
     res.render('Pedidos/registroPedidos', {
         titulo: 'CosmeTIC | Nuevo Pedido',
